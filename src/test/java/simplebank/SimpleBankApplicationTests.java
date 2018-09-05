bb3d0fd3c1c618744c666ee43fba842c729dbf76
Maxim Zinyakov <zinmv@rambler.ru>
2018-09-05 22:29:15 +0300

    simple-bank
    add another one test

--- src/test/java/simplebank/SimpleBankApplicationTests.java
+++ src/test/java/simplebank/SimpleBankApplicationTests.java
@@ -2,14 +2,17 @@ package simplebank;
 
 
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import simplebank.model.Account;
@@ -21,17 +24,39 @@ import simplebank.repository.TransactionRepository;
 import static org.mockito.Matchers.refEq;
 
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.iterableWithSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SimpleBankApplicationTests {
+
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
+                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
+
+        Assert.assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
 
 
     @Autowired
@@ -95,6 +120,27 @@ public class SimpleBankApplicationTests {
 
         mockMvc.perform(get("/api/transactions"))
                 .andExpect(jsonPath("$.*.transactionId", iterableWithSize(1)));
+    }
+
+
+
+    @Test
+    public void apiControllerPostCustomerShouldReturnCustomer() throws Exception {
+        String customerJson = json(new Customer(
+                "name", "address"));
+
+        mockMvc.perform(post("/api/postcustomer")
+                .contentType(contentType)
+                .content(customerJson))
+                .andExpect(status().isCreated());
+    }
+
+
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 
 
